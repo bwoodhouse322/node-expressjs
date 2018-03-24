f18fc1aa6c67d930216cec00ac91a5fff9ac9a8d
Ben.Woodhouse16 <ben.woodhouse@qa.com>
2018-03-24 17:05:24 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const hostname = "127.0.0.1";
+const port = 8080;
+
+var app = express();
+
+// var logger = (req,res,next) =>{
+    
+//     console.log("logging..");
+//     next();
+
+// }
+
+// app.use(logger);
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
+
+// app.use(express.static(path.join(__dirname,"client")));
+
+var people = ["greg","ben","jeff"];
+
+
+app.get('/',(req,res)=>{
+    res.render("index",{
+        title: people
+    });
+});
+
+app.listen(port,hostname,() =>{
+
+    console.log("listening on port" + port);
+
+});
